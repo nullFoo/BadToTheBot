5bcdf708af623214ca9f8ec525833bc603b06222
nullFoo <38643612+nullFoo@users.noreply.github.com>
2023-02-14 19:06:53 +1300

    changed order of loops

--- bot.py
+++ bot.py
@@ -12,25 +12,25 @@ async def on_ready():
 
 @tasks.loop(seconds=300)
 async def check_vc():
-    if(randint(0,5) == 1): # randomly
         for guild in client.guilds:
             if(guild is None):
                 print("server not found")
                 return;
-            print(guild)
-            print(len(guild.members))
-            print(guild.member_count)
-            for m in guild.members:
-                print(m)
-                if(not(m.voice is None)):
-                    print("is in voice")
-                    voice_channel = m.voice.channel;
-                    vc = await voice_channel.connect()
-                    vc.play(discord.FFmpegPCMAudio(secrets.path + '\\audio.mp3'), after=lambda e: print('done', e))
-                    # Sleep while audio is playing.
-                    while vc.is_playing():
-                        sleep(.1)
-                    await vc.disconnect()
-                    break;
+            if(randint(0,5) == 1): # randomly
+                print(guild)
+                print(len(guild.members))
+                print(guild.member_count)
+                for m in guild.members:
+                    print(m)
+                    if(not(m.voice is None)):
+                        print("is in voice")
+                        voice_channel = m.voice.channel;
+                        vc = await voice_channel.connect()
+                        vc.play(discord.FFmpegPCMAudio(secrets.path + '\\audio.mp3'), after=lambda e: print('done', e))
+                        # Sleep while audio is playing.
+                        while vc.is_playing():
+                            sleep(.1)
+                        await vc.disconnect()
+                        break;
 
 client.run(secrets.token)
