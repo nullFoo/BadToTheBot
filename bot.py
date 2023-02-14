c761ca4295cb4d69ebdebcb8ef513a59a463fab6
nullFoo <38643612+nullFoo@users.noreply.github.com>
2023-02-14 19:05:18 +1300

    made it work on multiple servers + moved token to separate file

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands, tasks
 from random import randint, choice
 from time import sleep
+import secrets
 client = discord.Client(intents = discord.Intents.all())
 
 @client.event
@@ -12,24 +13,24 @@ async def on_ready():
 @tasks.loop(seconds=300)
 async def check_vc():
     if(randint(0,5) == 1): # randomly
-        guild = client.get_guild(810395486874894348)
-        if(guild is None):
-            print("server not found")
-            return;
-        print(guild)
-        print(len(guild.members))
-        print(guild.member_count)
-        for m in guild.members:
-            print(m)
-            if(not(m.voice is None)):
-                print("is in voice")
-                voice_channel = m.voice.channel;
-                vc = await voice_channel.connect()
-                vc.play(discord.FFmpegPCMAudio('C:\\Users\\Amir\\Desktop\\Serious-Programming\\Stuff\\Bots\\BadToTheBone\\audio.mp3'), after=lambda e: print('done', e))
-                # Sleep while audio is playing.
-                while vc.is_playing():
-                    sleep(.1)
-                await vc.disconnect()
-                break;
+        for guild in client.guilds:
+            if(guild is None):
+                print("server not found")
+                return;
+            print(guild)
+            print(len(guild.members))
+            print(guild.member_count)
+            for m in guild.members:
+                print(m)
+                if(not(m.voice is None)):
+                    print("is in voice")
+                    voice_channel = m.voice.channel;
+                    vc = await voice_channel.connect()
+                    vc.play(discord.FFmpegPCMAudio(secrets.path + '\\audio.mp3'), after=lambda e: print('done', e))
+                    # Sleep while audio is playing.
+                    while vc.is_playing():
+                        sleep(.1)
+                    await vc.disconnect()
+                    break;
 
-client.run('MTA3NDkyNTUxNzA3MDkzNDA2Ng.G49lCY.FK3aV0IKJSmU92WT3HZUt9LuAeBBFuulhPciZM')
+client.run(secrets.token)
