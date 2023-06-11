0331099ecc161da377c0c2b1fc2e70933f069b28
Richie Bendall <richiebendall@gmail.com>
2023-06-11 16:47:17 +1200

    Minor tweaks

--- bot.py
+++ bot.py
@@ -2,9 +2,10 @@ import os
 import random
 import discord
 from discord.ext import commands, tasks
-from random import randint, choice
 from time import sleep
 import secrets
+
+# TODO: Make intents more specific
 client = discord.Client(intents = discord.Intents.all())
 
 dirname = os.path.dirname(os.path.realpath(__file__))
@@ -18,30 +19,36 @@ print(f"{len(sounds)} sounds found!")
 
 @client.event
 async def on_ready():
-    print('{0.user} Active'.format(client))
+    print('{0.user} active'.format(client))
     check_vc.start();
 
 @tasks.loop(seconds=300)
 async def check_vc():
         for guild in client.guilds:
-            if(guild is None):
-                print("server not found")
-                return;
-            if(randint(0,5) == 1): # randomly
+            if guild is None:
+                print("Server not found")
+
+                continue
+
+            if random.randint(0, 5) == 1: # 1 in 6 chance
                 print(guild)
                 print(len(guild.members))
                 print(guild.member_count)
-                for m in guild.members:
-                    print(m)
-                    if(not(m.voice is None)):
-                        print("is in voice")
-                        voice_channel = m.voice.channel;
-                        vc = await voice_channel.connect()
-                        vc.play(discord.FFmpegPCMAudio(random.choice(sounds)), after=lambda e: print('done', e))
-                        # Sleep while audio is playing.
-                        while vc.is_playing():
-                            sleep(.1)
-                        await vc.disconnect()
-                        break;
+
+                for member in guild.members:
+                    print(member)
+
+                    if member.voice is not None:
+                        print("Member is in voice channel")
+
+                        vc = await member.voice.channel.connect()
+                        
+                        def after_playing():
+                            print("Finished playing")
+                            vc.disconnect()
+                        
+                        vc.play(discord.FFmpegPCMAudio(random.choice(sounds)), after=lambda _: after_playing())
+
+                        break
 
 client.run(secrets.token)
