2ff428c2742f5a693e7b7eb33c1cc13fa25edd26
nullFoo <38643612+nullFoo@users.noreply.github.com>
2023-06-12 19:07:11 +1200

    select from channels instead of members, and now actually works

--- bot.py
+++ bot.py
@@ -24,33 +24,39 @@ async def on_ready():
 
 @tasks.loop(seconds=300)
 async def check_vc():
-        for guild in client.guilds:
-            if guild is None:
-                print("Server not found")
+    for guild in client.guilds:
+        if guild is None:
+            print("Server not found")
 
-                continue
+            continue
 
-            if random.randint(0, 5) == 1: # 1 in 6 chance
-                print(guild)
-                print(len(guild.members))
-                print(guild.member_count)
+        print(guild.name)
 
-                # TODO: Iterate over voice channels, not members
-                for member in guild.members:
-                    print(member)
+        if True:#random.randint(0, 5) == 1: # 1 in 6 chance
+            print("    Checking channels in server: " + guild.name)
+            inhabitedChannels = []
+            for channel in guild.voice_channels: # loop through channels in server
 
-                    if member.voice is not None:
-                        print("Member is in voice channel")
+                if len(channel.members) > 0: # if it has people in it, add it to a list
+                    print("           " + channel.name + " has " + str(len(channel.members)) + " members")
+                    inhabitedChannels.append(channel)
+            
+            if len(inhabitedChannels) > 0: # if at least one of the channels has people
+                channel: discord.guild.VoiceChannel = random.choice(inhabitedChannels) # pick a random inhabited channel
+                
+                print("Joining channel: " + channel.name + " in guild " + guild.name)
+                vc = await channel.connect(timeout=5, reconnect=False) # join it
+                
+                print("Connected. Playing...")
+                vc.play(discord.FFmpegPCMAudio(random.choice(sounds))) # play the sound
 
-                        vc = await member.voice.channel.connect()
-                        
-                        vc.play(discord.FFmpegPCMAudio(random.choice(sounds)))
-
-                        # Sleep while audio is playing.
-                        while vc.is_playing():
-                            sleep(.1)
-                        await vc.disconnect()
-
-                        break
+                # Sleep while audio is playing.
+                while vc.is_playing():
+                    sleep(.1)
+                
+                print("Finished playing")
+                await vc.disconnect() # then disconnect when done
+    print("Done")
+    print("")
 
 client.run(secrets.token)
